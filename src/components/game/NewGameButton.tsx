d9b30ffc10c4ba14e8970f89e60f65dcf8199e64
Trevor Coleman <trevor@trevorcoleman.design>
2021-04-27 14:46:05 -0400

    move onFail declaration to destructuring & reformat.

--- src/components/game/NewGameButton.tsx
+++ src/components/game/NewGameButton.tsx
@@ -7,52 +7,61 @@ import { useTypedSelector } from '../../store';
 import { Redirect } from 'react-router-dom';
 import { Game } from '../../store/Schema';
 
-
 interface NewGameButtonProps {
-  onCreate:(newKey: string)=>void;
-  onFail?:(message:string)=>void;
-  navigate?:boolean;
+  onCreate: (newKey: string) => void;
+  onFail?: (message: string) => void;
+  navigate?: boolean;
   template?: Partial<Game>;
   disabled?: boolean;
 }
 
 //COMPONENT
 const NewGameButton: FunctionComponent<NewGameButtonProps> = (props: NewGameButtonProps) => {
-  const {onCreate, template, ...rest} = props;
-  const classes=useStyles();
-  const onFail = props.onFail ? props.onFail : (message?:string)=>console.error(`Create game error: ${message ?? 'Failed to Create Game'}` )
-  const firebase = useFirebase()
+  const {
+    onCreate,
+    template,
+    onFail = (message?: string) => console.error(`Create game error: ${message ??
+                                                                       'Failed to Create Game'}`),
+    ...rest
+  } = props;
+  const classes = useStyles();
+  const firebase = useFirebase();
   const auth = useTypedSelector(state => state.firebase.auth);
-  const [key, setKey] = useState<string|null>(null);
+  const [key, setKey] = useState<string | null>(null);
 
-  const create = async ()=>{
+  const create = async () => {
 
     const newGame = {
       owner: auth.uid,
-      name: "New Game",
-      ...template
+      name : "New Game", ...template,
+
     };
 
     newGame.sort_name = newGame.name.toLowerCase();
 
+    const gameRef = await firebase.push('/games/', newGame);
 
-    const gameRef = await firebase.push('/games/',newGame)
-
-    if(gameRef.key) {
-      await firebase.ref(`/profiles/${auth.uid}/games/`).update({[gameRef.key]: true})
+    if (gameRef.key) {
+      await firebase.ref(`/profiles/${auth.uid}/games/`)
+                    .update({[gameRef.key]: true});
       onCreate(gameRef.key);
       setKey(gameRef.key);
-    }else {
+    }
+    else {
       onFail('Failed to fetch game key');
     }
 
-  }
-
+  };
 
   return (
       <div className={classes.root}>
-        <Button  variant="contained" color={"primary"} onClick={create} {...rest}>Create Game</Button>
-        {key?<Redirect to={`/game/${key}`}/>:""}</div>);
+        <Button
+            variant="contained"
+            color={"primary"}
+            onClick={create} {...rest}>Create Game</Button>
+        {key
+         ? <Redirect to={`/game/${key}`} />
+         : ""}</div>);
 };
 
 const useStyles = makeStyles((theme: Theme) => (
