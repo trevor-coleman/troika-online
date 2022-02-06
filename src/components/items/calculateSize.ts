a4dfb8788402796408f214a03234792ef847c0f6
Trevor Coleman <trevor@trevorcoleman.design>
2022-02-05 22:35:45 -0500

    fix bug with improper calculation of custom size

--- src/components/items/calculateSize.ts
+++ src/components/items/calculateSize.ts
@@ -1,97 +1,85 @@
-import { FormValueChange } from './forms/FormValueChange';
-import { Item } from '../../store/Item';
+import {FormValueChange} from './forms/FormValueChange';
+import {Item} from '../../store/Item';
 
-export function calculateSize(values: Item, update: FormValueChange<any>) {
-  const sizeOverride: Partial<Item> = {};
-  const {
-    id,
-    value,
-    source,
-  } = update;
+export function calculateSize(currentValues: Item, update: FormValueChange<any>) {
+    const sizeOverride: Partial<Item> = {};
+    const {
+        id,
+        value,
+        source,
+    } = update;
 
-  function twoHandedSize(isTwoHanded: boolean) {
+    function twoHandedSize(isTwoHanded: boolean) {
 
-    return isTwoHanded
-           ? Math.max(2,
-                      (
-                          values.protects && values.protection)
-                      ? values.protection * -2
-                      : 0,
-                      1)
-           : Math.max((
-                          values.protects && values.protection)
-                      ? values.protection * -2
-                      : 0, 1);
-  }
-
-  function protectionSize(protects: boolean, protection: number) {
-
-    return protects
-           ? Math.max(1,
-
-                      values.doesDamage && values.twoHanded
-                      ? 2
-                      : 0,
-
-                      protects && protection
-                      ? protection * -2
-                      : 0)
-
-           : Math.max(1,
-                      (
-                          values.doesDamage && values.twoHanded)
-                      ? 2
-                      : 0);
-  }
-
-  //reject changes if custom size is enabled
-  if (values.customSize && id === "size" && source !== "customSizeSection")
-  {
-    sizeOverride.size = value.size;
-  }
-
-  //recalculate size if disabling custom size
-  else if (id === "customSize" && !value)
-  {
-    sizeOverride.size =
-        Math.max(values.twoHanded
-                 ? 2
-                 : 0,
-                 values.protects && values.protection
-                 ? values.protection * -2
-                 : 0,
-                 1);
-  }
-
-  else if (id === "doesDamage")
-  {
-    if (value && values.twoHanded)
-    {
-      sizeOverride.size = twoHandedSize(values.twoHanded);
+        return isTwoHanded
+            ? Math.max(2,
+                (
+                    currentValues.protects && currentValues.protection)
+                    ? currentValues.protection * -2
+                    : 0,
+                1)
+            : Math.max((
+                currentValues.protects && currentValues.protection)
+                ? currentValues.protection * -2
+                : 0, 1);
     }
-    else
-    {
-      sizeOverride.size = twoHandedSize(false);
-      console.log(sizeOverride);
+
+    function protectionSize(protects: boolean, protection: number) {
+
+        return protects
+            ? Math.max(1,
+
+                currentValues.doesDamage && currentValues.twoHanded
+                    ? 2
+                    : 0,
+
+                protects && protection
+                    ? protection * -2
+                    : 0)
+
+            : Math.max(1,
+                (
+                    currentValues.doesDamage && currentValues.twoHanded)
+                    ? 2
+                    : 0);
     }
-  }
 
-  //adjust size if needed for two handed weapons
-  else if (id === "twoHanded" && !values.customSize)
-  {
-    sizeOverride.size = twoHandedSize(value);
-  }
+    //reject changes if custom size is enabled
+    if (currentValues.customSize && id === "size") {
 
-  else if (id == "protects" && !values.customSize)
-  {
-    sizeOverride.size = protectionSize(value, values.protection ?? 0);
-  }
+        sizeOverride.size = source === "customSizeSection" ? value.size : currentValues.size;
+    }
 
-  //adjust size if needed for armour
-  else if (id === "protection" && !values.customSize)
-  {
-    sizeOverride.size = protectionSize(values.protects, value);
-  }
+    //recalculate size if disabling custom size
+    else if (id === "customSize" && !value) {
+        sizeOverride.size =
+            Math.max(currentValues.twoHanded
+                    ? 2
+                    : 0,
+                currentValues.protects && currentValues.protection
+                    ? currentValues.protection * -2
+                    : 0,
+                1);
+    } else if (id === "doesDamage") {
+        if (value && currentValues.twoHanded) {
+            sizeOverride.size = twoHandedSize(currentValues.twoHanded);
+        } else {
+            sizeOverride.size = twoHandedSize(false);
+            console.log(sizeOverride);
+        }
+    }
 
-  return sizeOverride;
+    //adjust size if needed for two handed weapons
+    else if (id === "twoHanded" && !currentValues.customSize) {
+        sizeOverride.size = twoHandedSize(value);
+    } else if (id == "protects" && !currentValues.customSize) {
+        sizeOverride.size = protectionSize(value, currentValues.protection ?? 0);
+    }
+
+    //adjust size if needed for armour
+    else if (id === "protection" && !currentValues.customSize) {
+        sizeOverride.size = protectionSize(currentValues.protects, value);
+    }
+
+    return sizeOverride;
 }
